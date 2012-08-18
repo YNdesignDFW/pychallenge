a994b6a837f3e160d3429e0dbf05af6a32a99225
bekindtomonkeys <jared.caraway@gmail.com>
2012-08-17 19:54:55 -0500

    Got it running, I'm getting close to the answer.
    Got the program to autoload webpages and scan for the next number to
    append to the web address to load the next page, etc. But I feel like I
    should figure it out with a bit more tinkering.

--- lvl-4/lvl-4.py
+++ lvl-4/lvl-4.py
@@ -1,10 +1,26 @@
 import urllib2
 
-def getPage(bs):
-	url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + str(bs)
+def getPage(endString):
+	#This function takes the standard ?nothing= URL suffix and adds on endString
+	#Found by autoloading and searching each page for a numeric value
+	url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + str(endString)
 	print url
 	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
 	return response.read()
 	
-print getPage("12345")
+
+newPage = getPage("12345")
+nextPage = ""
+x = 0
+while x <= 400:
+	
+	for item in newPage:
+		if not item.isdigit():
+			nextPage = ""
+		elif item.isdigit():
+			nextPage += item
+	print nextPage
+	newPage = getPage(nextPage)
+	x += 1
+	print "iteration \#" + str(x)
